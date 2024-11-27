72fc3602d923550986122513a61854926c24fe41
darkicewolf50 <95242911+darkicewolf50@users.noreply.github.com>
2024-11-27 10:50:06 -0700

    feat(Packagers): testing out Django response options

--- djangotutorial/mysite/newtest.py
+++ djangotutorial/mysite/newtest.py
@@ -4,8 +4,14 @@ from django.http import HttpResponse
 def index(request):
     ymlschedule = {"message": False}
     res = {
-            "statusCode": 200,
+            #"statusCode": 200, commented out because not needed django/wrapper will handle it
             "isBase64ENcoded": "false",
             "body": json.dumps(ymlschedule)
         }
-    return HttpResponse(json.dumps(res))
+    return HttpResponse(json.dumps(res), content_type='application/json')
+    '''
+    response =  HttpResponse(json.dumps(res), content_type='application/json')
+    response['X-IsBase64Encoded'] = 'false'  # Custom header to indicate base64 encoding
+    response.status_code = 200
+    return response
+    '''
