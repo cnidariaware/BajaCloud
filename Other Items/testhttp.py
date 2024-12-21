1f244d4d625030d95a09577a32a230450535394b
darkicewolf50 <darkicewolf50@gmail.com>
2024-12-21 14:29:43 -0700

    fix (testhttp): fixed linting errors

--- Other Items/testhttp.py
+++ Other Items/testhttp.py
@@ -5,12 +5,12 @@ import timeit
 def BenchMarkServer():
     rawRes = requests.get("http://localhost:8080/getAppointments")
     res = json.loads(rawRes.text)
-    #print(json.dumps(res, indent=1))
+    print(json.dumps(res, indent=1))
 
 def BenchMarkDjango():
     rawRes = requests.get("http://127.0.0.1:8000/getAppointments")
     res = json.loads(rawRes.text)
-    #print(json.dumps(res, indent=1))
+    print(json.dumps(res, indent=1))
 
 if __name__ == "__main__":
     test = 1
@@ -19,7 +19,7 @@ if __name__ == "__main__":
         # pythonTime = timeit.timeit(stmt=BenchMarkServer, number=10)
         print(f"FastAPI: {djangoTime}\nPython: ")
     
-    reqbody = {
-        "body": {"message": "hello"}
-    }
+    # reqbody = {
+    #     "body": {"message": "hello"}
+    # }
     # rawRes = requests.post("http://localhost:8000/SelectInterview", reqbody)
