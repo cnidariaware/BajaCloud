f4dc2d0f4fcfc8ce9555817953dbe4674ee09209
darkicewolf50 <darkicewolf50@gmail.com>
2025-01-25 13:30:51 -0700

    removed(main): removed unused middlewares

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from InterviewBooking.NoSheet import NoSheet
