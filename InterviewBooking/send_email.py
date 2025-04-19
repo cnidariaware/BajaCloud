f333759a576c3f8bf0a6234b7dc73a87fcc936cf
darkicewolf50 <darkicewolf50@gmail.com>
2025-04-19 15:05:35 -0600

    feat(email sender): started to fix email css

--- InterviewBooking/send_email.py
+++ InterviewBooking/send_email.py
@@ -45,38 +45,94 @@ def send_email(interviewee_email="darkicewolf50@gmail.com", interviewee_name="br
 
     # Message body
     body = f'''
-    <html lang="en-US">
-      <head>
-        <title>Interview Invitation</title>
-      </head>
-      <body>
-        <p>Dear {interviewee_name},</p>
-        <p>Your interview has been scheduled on {date} at {start_time} MST.</p>
-        <p> Your interview location is at {location} or will be emailed to you.  </p>
-        <p>Please ensure to be available at the designated time.</p>
-        <a
-          href="https://calendar.google.com/calendar/render?action=TEMPLATE&text=UCalgary+Baja+Interview+with+{interviewee_name}&dates={start_time_google}/{end_time_google}&details=Interview+with+UCalgary+Baja+Team&location={location}"
-          target="_blank"
-        >
-          <button type="button" class="AddtoCal">Add to Google Calendar</button>
-        </a>
-        <a
-          href="https://outlook.live.com/calendar/0/deeplink/compose?subject=UCalgary+Baja+Interview+with+{interviewee_name}&body=Interview+with+UCalgary+Baja+Team&location={location}&startdt={outlook_start}&enddt={outlook_end}"
-          target="_blank"
-        >
-          <button type="button" class="AddtoCal">Add to Outlook Calendar</button>
-        </a>
-        <p>Best regards,</p>
-        <p>UCalgary Baja Interview Team</p>
-        <img
-          src="https://res.cloudinary.com/dpgrgsh7g/image/upload/v1733003224/UCalgaryBAJA_Logo-2024_mpmljh.png"
-          alt="UCalgary Baja Team"
-          height="120svh"
-        />
-      </body>
-    </html>
-    '''
-
+<html lang="en-US">
+	<head>
+		<title>Interview Invitation</title>
+	</head>
+	<body>
+		<p>Dear {interviewee_name},</p>
+		<p>Your interview has been scheduled on {date} at {start_time}</p>
+		<p>Please ensure to be available at the designated time.</p>
+		<table
+			role="presentation"
+			border="0"
+			cellpadding="0"
+			cellspacing="0"
+			style="width: 100%; margin: 20px 0">
+			<tr>
+				<td
+					align="center"
+					style="width: 48%; margin-right: 1%">
+					<a
+						href="https://calendar.google.com/calendar/render?action=TEMPLATE&text=Interview+with+{interviewee_name}&dates=20241130T100000Z/20241130T110000Z&details=Interview+with+UCalgary+Baja+Team&location=ENC37"
+						target="_blank"
+						style="
+							text-decoration: none;
+							display: inline-block;
+							background-color: #376fc9;
+							color: white;
+							padding: 10px 20px;
+							font-family: Arial, sans-serif;
+							font-size: 14px;
+							border-radius: 4px;
+							border: 2px solid grey;
+						">
+						<img
+							src="https://res.cloudinary.com/dpgrgsh7g/image/upload/v1745094071/google-calendar-svgrepo-com_vkmq2n.png"
+							alt="Google Calendar"
+							style="
+								vertical-align: middle;
+								height: 40px;
+								margin-right: 10px;
+							" />
+						Add to Google Calendar
+					</a>
+				</td>
+				<td
+					align="center"
+					style="width: 48%; margin-left: 1%">
+					<a
+						href="https://outlook.live.com/calendar/0/deeplink/compose?subject=Interview+with+{interviewee_name}&body=Interview+with+UCalgary+Baja+Team&location=ENC37&startdt=2024-11-30T10:00:00&enddt=2024-11-30T11:00:00"
+						target="_blank"
+						style="
+							text-decoration: none;
+							display: inline-block;
+							background-color: #376fc9;
+							color: white;
+							padding: 10px 20px;
+							font-family: Arial, sans-serif;
+							font-size: 14px;
+							border-radius: 4px;
+							border: 2px solid grey;
+						">
+						<img
+							src="https://res.cloudinary.com/dpgrgsh7g/image/upload/v1745094071/ms-outlook-svgrepo-com_idmyw0.png"
+							alt="Outlook Logo"
+							style="
+								vertical-align: middle;
+								height: 40px;
+								margin-right: 10px;
+							" />
+						Add to Outlook Calendar
+					</a>
+				</td>
+			</tr>
+		</table>
+		<p>Best regards,</p>
+		<p>UCalgary Baja Interview Team</p>
+		<img
+			src="https://res.cloudinary.com/dpgrgsh7g/image/upload/v1733003224/UCalgaryBAJA_Logo-2024_mpmljh.png"
+			alt="UCalgary Baja Logo"
+			height="120svh" />
+		<p>
+			If you need to change the date or cancel please email
+			<a href="mailto:uofcbaja@gmail.com">uofcbaja@gmail.com</a>
+			or reply all to this email
+		</p>
+	</body>
+</html>
+'''
+    
     msg.attach(MIMEText(body, 'html'))
 
     try:
